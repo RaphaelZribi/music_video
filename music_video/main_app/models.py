cff733013ca1955a327365a5ef020e1690f0c461
Sharon Chetrit <sharon.chetrit@hotmail.fr>
2018-12-17 09:57:20 +0200

    import json and api key

--- music_video/main_app/models.py
+++ music_video/main_app/models.py
@@ -3,13 +3,3 @@ from django.contrib.auth.models import User
 
 
 
-class Video(models.Model):
-	
-
-class Category(models.Model):
-
-class Comment(models.Model):
-
-
-class Profile(models.Model):
-	user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
