cf1fbec7d82298681f3bd83f5628af9d9d87ef4e
Sharon Chetrit <sharon.chetrit@hotmail.fr>
2018-12-18 11:48:32 +0200

    display playlists

--- music_video/main_app/views.py
+++ music_video/main_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main_app.models import Playlist, Video
+
 
 # Create your views here.
-def index(request):
-	return HttpResponse('salut les gars')
+def home(request):
+	playlists = Playlist.objects.all()
+	print(playlists)
+	# videos = Video.objects.all().order_by('-date')[:10]
+
+	return render(request, 'home.html', { 'playlists': playlists})
