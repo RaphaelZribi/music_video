49a568c11d52a439bd5c9ad4fa4eb37d5912f00e
RaphZribi <raphaelzribi1@gmail.com>
2018-12-18 10:01:50 +0200

    populate all

--- music_video/main_app/models.py
+++ music_video/main_app/models.py
@@ -1,30 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
+#from datetime import datetime
+from django.core.validators import URLValidator
+from django.utils import timezone
 
 
 
-class Video(models.Model):
-	title = models.CharField(max_length=400)
-	url = models.CharField(max_length=2000, validators=[URLValidator()])
-	category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 class Category(models.Model):
 	name = models.CharField(max_length=200)
 
-class Comment(models.Model):
-	userprofileinfo = models.ForeignKey(Profile, on_delete=models.CASCADE)
-	text = models.TextField()
-	date = models.DateTimeField(default=datetime.now())
-	video = models.ForeignKey(Video, on_delete=models.CASCADE)
-
+class Playlist(models.Model):
+	playlist_id = models.CharField(max_length=400, unique=True)
+	title       = models.CharField(max_length=400)
+	description = models.CharField(max_length=400)
+	thumbnail   = models.CharField(max_length=2000, validators=[URLValidator()])
 
 	def __repr__(self):
-		return "<Comment of {}>".format(self.userprofileinfo.user.username)
+		return "<{}>".format(self.title)
 
 	def __str__(self):
-		return "{}".format(self.userprofileinfo.user.username)
+		return "{}".format(self.title)
 
+class Video(models.Model):
+	video_id    = models.CharField(max_length=2000, unique=True)
+	title       = models.CharField(max_length=2000)
+	description = models.CharField(max_length=2000)
+	thumbnail   = models.CharField(max_length=2000, validators=[URLValidator()], default=None)
+	playlist    = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+	#category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -37,3 +41,16 @@ class Profile(models.Model):
 	def __str__(self):
 		return '{} {}'.format(self.user.first_name, self.user.last_name)
 
+
+class Comment(models.Model):
+	userprofileinfo = models.ForeignKey(Profile, on_delete=models.CASCADE)
+	text = models.TextField()
+	date = models.DateTimeField(default=timezone.now())
+	video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+
+	def __repr__(self):
+		return "<Comment of {}>".format(self.userprofileinfo.user.username)
+
+	def __str__(self):
+		return "{}".format(self.userprofileinfo.user.username)
--- music_video/populate_videos.py
+++ music_video/populate_videos.py
@@ -1,14 +1,63 @@
 import requests
 import json
 
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_video.settings')
+import django
+django.setup()
+from main_app.models import Playlist, Video
+
 
 api_key = 'AIzaSyDK5Fjp3r91iQzNW-jaoVeuTszPIA98jJ0'
 channel_id = 'UCPVhZsC2od1xjGhgEc2NEPQ'
-api_url = 'https://www.googleapis.com/youtube/v3/search?key={}&channelId={}&part=snippet,id&order=date&maxResults={}'.format(api_key, channel_id, 20)
-
-def get_videos(api_url):
-	videos = requests.get(api_url).json()
-	print(videos)
+playlists_api_url = 'https://www.googleapis.com/youtube/v3/search?key={}&channelId={}&part=snippet,id&order=date&maxResults={}'.format(api_key, channel_id, 20)
 
 
-get_videos(api_url)
+def get_videos(playlist):
+	
+	playlist_api_url= 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults={}&playlistId={}&key={}'.format(20, playlist.playlist_id, api_key)
+	playlist_json = requests.get(playlist_api_url).json()
+	playlist_items = playlist_json['items']
+
+	for dico in playlist_items:
+		video_id =dico['id']
+		title = dico['snippet']['title']
+		description= dico['snippet']['description']
+		thumbnail = dico['snippet']['thumbnails']['default']['url']
+		video = Video.objects.get_or_create(video_id = video_id,
+											title = title,
+											description = description,
+											thumbnail = thumbnail, 
+											playlist= playlist)[0]
+		
+
+
+def get_playlists(playlists_api_url):
+	playlist_json = requests.get(playlists_api_url).json()
+	playlists_items = playlist_json['items']
+	for item in playlists_items: 
+		if 'playlistId' in item['id']:
+			playlist_id = item['id']['playlistId']
+			title = item['snippet']['title']
+			description= item['snippet']['description']
+			thumbnail = item['snippet']['thumbnails']['default']['url']
+		
+		
+
+		playlist = Playlist.objects.get_or_create(playlist_id= playlist_id, 
+													  title=title, 
+													  description= description,
+													  thumbnail=thumbnail)[0]
+		get_videos(playlist)
+			
+		
+
+
+if __name__ == '__main__':
+    print('Staring to populate...')
+    get_playlists(playlists_api_url)
+
+    print('Finished populating!')
+
+
+
